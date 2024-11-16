e8f2e251d9542da205deacd7d54ed0019e1ff32b
Frenzy02 <chrisvallejos0204@gmail.com>
2024-11-16 19:42:14 +0800

    Fix missing modules and import paths

--- src/app/student-panel/page.jsx
+++
@@ -1,8 +0,0 @@
-// src/app/student-panel.jsx
-import StudentPanel from '../components/StudentPanel/page'
-
-const StudentPanelPage = () => {
-    return <StudentPanel />
-}
-
-export default StudentPanelPage
